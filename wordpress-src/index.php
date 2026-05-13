e5b15110108bbea48189ab7ed9efc4414f620689
aarvvnn02 <aarongithubdamb@gmail.com>
2026-05-13 17:41:09 +0200

    Prueba actualizacion automatica

--- wordpress-src/index.php
+++ wordpress-src/index.php
@@ -1,6 +1,6 @@
 <?php
 
-echo "<h1>Proyecto automatizado funcionando</h1>";
+echo "<h1>CAMBIO AUTOMATICO FUNCIONANDO</h1>";
 
 echo "<p>WordPress + Docker + Vagrant funcionando correctamente.</p>";
 
