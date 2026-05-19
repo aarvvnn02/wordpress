c5779b22ad1a95aba36a7d4e35bd035cf3f22b1a
aaron <alumnotd@max12>
2026-05-19 10:08:05 +0200

    Cambios realizados

--- wordpress-src/index.php
+++ wordpress-src/index.php
@@ -1,6 +1,6 @@
 <?php
 
-echo "<h1>CAMBIO AUTOMATICO FUNCIONANDO</h1>";
+echo "<h1>CAMBIO AUTOMATICO FUNCIONANDO CORRECTAMENTE</h1>";
 
 echo "<p>WordPress + Docker + Vagrant funcionando correctamente.</p>";
 
