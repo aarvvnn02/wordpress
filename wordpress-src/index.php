9f42a429bba4b8bdae72a5f8b4376479413ac013
aarvvnn02 <aarongithubdamb@gmail.com>
2026-05-19 10:44:29 +0200

    Modify index.php
    Updated the index.php to display new success messages.

--- wordpress-src/index.php
+++ wordpress-src/index.php
@@ -1,19 +1,13 @@
 <?php
-get_header();
-?>
 
-<h1>Proyecto WordPress Automatizado</h1>
 
-<p>
-Esta página se despliega automáticamente usando:
-</p>
 
-<ul>
-    <li>Docker</li>
-    <li>Vagrant</li>
-    <li>GitHub</li>
-</ul>
+echo "<h1>CAMBIO AUTOMATICO FUNCIONANDO CORRECTAMENTE</h1>";
 
-<?php
-get_footer();
+
+
+echo "<p>WordPress + Docker + Vagrant funcionando correctamente.</p>";
+
+
+
 ?>
